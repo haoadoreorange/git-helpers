b28961f3d2e2702ef8da5c70ebb2ff0572794944
haoadoreorange <github@h4o.dev>
2022-07-23 09:42:53 -0400

    Update install script, log when already exist
    Signed-off-by: haoadoreorange <github@h4o.dev>

--- install.sh
+++ install.sh
@@ -6,16 +6,17 @@ NC='\033[0m' # No Color
 INSTALL_DIR="$(realpath "${1:-$HOME/.git-helpers}")"
 
 if [ ! -d "$INSTALL_DIR"/hooks ]; then
-    printf "${GREEN}Download git-helpers to %s${NC}\n" "$INSTALL_DIR"
+    printf "${GREEN}Downloading git-helpers to %s${NC}\n" "$INSTALL_DIR"
     git clone --recurse-submodules https://github.com/haoadoreorange/git-helpers "$INSTALL_DIR"
 else
+    printf "${GREEN}git-helpers already downloaded at %s, pulling newest commit${NC}\n" "$INSTALL_DIR"
     cd "$INSTALL_DIR"
     git pull
 fi
 
 for file in "$INSTALL_DIR"/hooks/*; do
     if [ -f "$file" ]; then
-        printf "${GREEN}Softlink git hook '%s' to /usr/share/git-core/templates/hooks/${NC}\n" "$(basename "$file")"
+        printf "${GREEN}Softlinking git hook '%s' to /usr/share/git-core/templates/hooks/${NC}\n" "$(basename "$file")"
         chmod +x "$file"
         sudo ln -s "$file" /usr/share/git-core/templates/hooks/ || failed=true
     fi
